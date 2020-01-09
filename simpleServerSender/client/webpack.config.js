941d83cf666716e2693b10c12fc880675fa4453b
Eric Laursen <ericjlaursen@gmail.com>
2020-01-08 21:09:34 -0500

    init frontend files

---
+++ simpleServerSender/client/webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCss = require("mini-css-extract-plugin");
+// const path = require('path')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCss.loader, "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|ttf)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCss({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
+};
