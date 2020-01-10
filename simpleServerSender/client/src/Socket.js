10e628fbbab511cb910109d7402f983c8a2ed95b
Eric Laursen <ericjlaursen@gmail.com>
2020-01-09 20:43:43 -0500

    2 buttons that emit events

--- simpleServerSender/client/src/Socket.js
+++ simpleServerSender/client/src/Socket.js
@@ -4,33 +4,47 @@ import socket from "socket.io-client";
 const useSocket = (serverUrl, topic) => {
   const [temp, setTemp] = React.useState(0);
   const [isConnected, setConnected] = React.useState(false);
-  console.log('isConnected')
-  console.log(isConnected)
+  const [socketClient, setSocketClient] = React.useState(null)
 
   React.useEffect(() => {
     if(!isConnected){
-      const client = socket.connect(serverUrl);
-      client.on("connect", () => setConnected(true));
-      client.on("disconnect", () => setConnected(false));
-      client.on(topic, data => {
+
+      let thisSocketClient = socket.connect(serverUrl);
+      thisSocketClient.on("connect", () => setConnected(true));
+      thisSocketClient.on("disconnect", () => setConnected(false));
+      thisSocketClient.on(topic, data => {
         setTemp(data);
       });
+      setSocketClient(thisSocketClient)
     }
   }, [topic]);
 
-  return { temp, isConnected };
+  return { temp, isConnected, socketClient };
 };
 
-const Sockt = ({ serverUrl, topic }) => {
-  const { temp, isConnected } = useSocket(serverUrl, topic);
+
+const SocketConsumer = ({ serverUrl, topic }) => {
+  const { temp, isConnected, socketClient } = useSocket(serverUrl, topic);
+
+  const clickHandler = () => {
+    console.log('clicked evA!');
+    socketClient.emit('evA')
+  }
+
+  const clickHandlerTwo = () => {
+    console.log('clicked evB!');
+    socketClient.emit('evB')
+  }
 
   return (
     <div>
       <h4>Temperature</h4>
       <h1>{temp}</h1>
       <h3>{`CONNECTED: ${isConnected}`}</h3>
+      <button onClick={clickHandler}>Send EventA!</button>
+      <button onClick={clickHandlerTwo}>Send EventB!</button>
     </div>
   );
 };
 
-export default Sockt;
+export default SocketConsumer;
